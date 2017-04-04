c7c532aee4c7e18757c511fb2d604035c45fd551
YouROK <8YouROK8@mail.ru>
2017-04-04 15:32:40 +0300

    set def dir

--- app/src/main/java/ru/yourok/loader/MyApplication.java
+++ app/src/main/java/ru/yourok/loader/MyApplication.java
@@ -2,6 +2,7 @@ package ru.yourok.loader;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Environment;
 
 /**
  * Created by yourok on 23.03.17.
@@ -19,6 +20,11 @@ public class MyApplication extends Application {
     public void onCreate() {
         context = getApplicationContext();
         Store.Init(this);
+        String dp = Manager.GetSettings().getDownloadPath();
+        if (dp.isEmpty()) {
+            dp = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+            Manager.SetSettingsDownloadPath(dp);
+        }
         super.onCreate();
     }
 }
