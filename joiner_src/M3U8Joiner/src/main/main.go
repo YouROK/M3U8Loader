d6421f851fed9c75426e5c49d9eb1d48306ec5bb
YouROK <8YouROK8@mail.ru>
2017-04-04 09:22:04 +0300

    tested

--- joiner_src/M3U8Joiner/src/main/main.go
+++ joiner_src/M3U8Joiner/src/main/main.go
@@ -13,9 +13,11 @@ func main() {
 	//	opt.Url = "http://api.new.livestream.com/accounts/22931184/events/6846118/videos/146115126.m3u8"
 	//	opt.Url = "http://atv-vod.ercdn.net/eskiya_dunyaya_hukumdar_olmaz/050/eskiya_dunyaya_hukumdar_olmaz_050.smil/chunklist_b1200000_t64MTIwMGticHM=.m3u8?st=UMUgMJhxlvjQEdm1fxfT_Q&e=1484251384&SessionID=1qhyziauy3dwxx4yxfydiu2m&StreamGroup=eskiya-dunyaya-hukumdar-olmaz&Site=atv&DeviceGroup=web"
 	//	opt.Url = "file:///home/yourok/Projects/Android/M3U8Loader/joiner_src/M3U8Joiner/src/main/a.m3u8"
-	opt.Url = "http://4pda.ru/pages/go/?u=http%3A%2F%2Fhlsarchive.info-link.ru%3A8191%2Fotr%2Findex-1485255600-300.m3u8%3Ftoken%3Dd382498e89dc0f75&e=57563231"
+	//	opt.Url = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"
+	//	opt.Url = "https://vootvideo.akamaized.net/enc/fhls/p/1982551/sp/198255100/serveFlavor/entryId/0_g4nuihuo/v/2/pv/1/flavorId/0_11554gdn/name/a.mp4/index.m3u8"
+	opt.Url = "http://localhost:8090/files/crypted/crypted.m3u8"
 
-	opt.Name = "test"
+	opt.Name = "crypted1"
 	opt.OutFileDir = opt.TempDir
 	opt.Threads = 30
 	opt.Timeout = 0
@@ -27,10 +29,10 @@ func main() {
 			err = m.LoadList()
 		}
 		if err == nil {
-			/*for i := 1; i < m.GetList().ListsSize(); i++ {
+			for i := 1; i < m.GetList().ListsSize(); i++ {
 				m.GetList().GetList(i).IsLoad = false
 			}
-			for i := 1; i < m.GetList().ItemsSize(); i++ {
+			/*for i := 1; i < m.GetList().ItemsSize(); i++ {
 				m.GetList().GetItem(i).IsLoad = false
 			}*/
 			for true {
