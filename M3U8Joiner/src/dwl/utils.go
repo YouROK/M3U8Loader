555aff76d52a702334e533c6a7e66ab84a80fd19
YouROK <8YouROK8@mail.ru>
2017-04-12 11:46:26 +0300

    add GetNumbersStr func

--- M3U8Joiner/src/dwl/utils.go
+++ M3U8Joiner/src/dwl/utils.go
@@ -4,6 +4,8 @@ import (
 	"dwl/list"
 	"dwl/load"
 	"net/http"
+	"strconv"
+	"unicode"
 )
 
 func (m *Manager) parse(url, name string) ([]*list.List, error) {
@@ -41,3 +43,39 @@ func (m *Manager) findLoaderName(name string) *load.Loader {
 	return nil
 }
 
+func GetNumbersStr(val string) int {
+	arrNums := make([]string, 0)
+	pos := 0
+	isFind := false
+	for _, n := range val {
+		if unicode.IsDigit(n) {
+			isFind = true
+			if len(arrNums)-1 < pos {
+				arrNums = append(arrNums, "")
+			}
+			arrNums[pos] += string(n)
+		}
+		if !unicode.IsDigit(n) {
+			if isFind {
+				pos++
+			}
+			isFind = false
+		}
+	}
+
+	if len(arrNums) == 0 {
+		return -1
+	}
+
+	num := 0
+	for i, n := range arrNums {
+		coff := (len(arrNums) - 1 - i) * 1000
+		if coff == 0 {
+			coff = 1
+		}
+		nn, _ := strconv.Atoi(n)
+		num += nn * coff
+	}
+
+	return num
+}
